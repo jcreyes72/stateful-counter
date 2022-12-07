07879fae3fea9e68152a78164c0fbfbd0cb0731e
Julio Reyes <jcreyes@g.clemson.edu>
2022-12-07 14:10:43 -0500

    Increment/Decrement Functionality
    Made the increment and decrement buttons work

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Counter.css'
 
 const Counter = () => {
+
+  const [count, setCount] = useState(0);
+
   return (
+  <div>
     <div className="grid-container">
         <div className="grid-item grid-item-1">
-            <button className="addOne">Increment</button>
+            <button className="button" onClick={() => setCount(count + 1)}>Increment</button>
         </div>
         <div className="grid-item grid-item-2">
-            <span className="number">0</span>
+            <span className="number">{count}</span>
         </div>
         <div className="grid-item grid-item-3">
-            <button className="subtractOne">Decrement</button>
+            <button className="button" onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
     </div>
+  </div>
 
   )
 }
